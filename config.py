1256f2252213aaaa2282e36960d77c518d6c65c8
Mugisha Thierry <thierrymugis@gmail.com>
2020-12-05 13:25:04 +0200

    Created a config file

---
+++ config.py
@@ -0,0 +1,35 @@
+import os
+
+
+
+class Config:
+
+    # NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?category={}&apiKey={}'
+    # ARTICLE_NEWS_URL ='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    # NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://thierry:Christine@99@localhost/pitch_project'
+    
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
