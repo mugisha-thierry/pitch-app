3fc8d49cc81a2fcca54b804da0e703e543e7c8d6
Mugisha Thierry <thierrymugis@gmail.com>
2020-12-06 12:38:30 +0200

    Created a coment form

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,3 +6,14 @@ from wtforms.validators import Required
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
+
+
+class PitchForm(FlaskForm):
+    title = StringField('Title', validators=[Required()])
+    category = SelectField('Category', choices=[('Hobbies','Hobbies'),('Experiences','Experiences'),('Skills','Skills')],validators=[Required()])
+    pitch = TextAreaField('Your Pitch...', validators=[Required()])
+    submit = SubmitField('Save')
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Comment here',validators=[Required()])
+    submit = SubmitField('Comment')
