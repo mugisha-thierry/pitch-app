7a9506706ff14e8ff961a802e9d3d8ed4bf77dd4
Mugisha Thierry <thierrymugis@gmail.com>
2020-12-06 12:43:39 +0200

    Added a sender email

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 
 export SECRET_KEY=thierry123
-# export MAIL_USERNAME=thierrymugis@gmail.com
-# export MAIL_PASSWORD=Christine@99
+export MAIL_USERNAME=thierrymugis@gmail.com
+export MAIL_PASSWORD=******
 
 python3.8 manage.py server
